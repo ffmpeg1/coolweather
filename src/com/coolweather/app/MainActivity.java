426307e5d3524ccab31f26bfcbd261521497653c
hw <hwhewei024@163.com>
2014-08-21 15:22:49 +0800

    First commit

---
+++ src/com/coolweather/app/MainActivity.java
@@ -0,0 +1,26 @@
+package com.coolweather.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	
+
+
+
+}
